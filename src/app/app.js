7f472b3e15a7330b40586cee6011f1f670c89b54
marcchik <markstal2002@gmail.com>
2022-02-25 15:37:33 +0300

    Начало курсача положено

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './app.css';
+
+export default class extends Component {
+
+    render() {
+        return(
+            <h1>Начало курсача положено</h1>
+        )
+    }
+
+}
